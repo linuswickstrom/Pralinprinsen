833b86128ae7cb6e464a429658190c9e462edb2c
linuswickstrom <58732846+linuswickstrom@users.noreply.github.com>
2020-02-11 12:25:42 +0100

    k

---
+++ js.js
@@ -0,0 +1,24 @@
+var state = 0;
+
+function myFunction() {
+  if (state == 0) {
+    document.getElementById("myDropdown").style.display = "block";
+    state = 1;
+  } else {
+    document.getElementById("myDropdown").style.display = "none";
+    state = 0;
+  }
+}
+
+window.onclick = function(event) {if
+(!event.target.matches('.dropdown')) {
+  var dropdowns = document.getElementsByClassName("dropdown");
+  var i;
+  for(i = 0; i <dropdowns.length; i++) {
+    var openDropdown = dropdowns [i];
+    if (openDropdown.classlist.contains('show')) {
+      openDropdown.classlist.remove('show');
+      }
+    }
+  }
+}
